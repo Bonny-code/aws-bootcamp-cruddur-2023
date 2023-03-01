6f1089de56b8d4b414a5f50e3302c35084b8e7e8
Bonny-code <ndiboniface@outlook.com>
2023-03-01 16:13:55 +0000

    corrected notifications page

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
-from services.notifications_activity import *
+from services.notifications_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -92,7 +92,7 @@ def data_home():
 
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
-  data = NotificationActivities.run()
+  data = NotificationsActivities.run()
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
