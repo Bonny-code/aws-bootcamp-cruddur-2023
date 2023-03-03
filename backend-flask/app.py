8e4edb67aeb48a5effb05f81bf642739c18da215
Bonny-code <ndiboniface@outlook.com>
2023-03-03 20:54:45 +0000

    added rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,6 +36,12 @@ import watchtower
 import logging
 from time import strftime
 
+#Rollbar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # Configuring Logger to Use CloudWatch
 #LOGGER = logging.getLogger(__name__)
 #LOGGER.setLevel(logging.DEBUG)
@@ -50,6 +56,8 @@ from time import strftime
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+
 
 #show this in the logs within the backend-flask app
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -86,6 +94,29 @@ cors = CORS(
 #   timestamp = strftime('[%Y-%b-%d %H:%M]')
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
+
+#Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 
 @app.route("/api/message_groups", methods=['GET'])
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,7 +3,7 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    segment = xray_recorder.begin_segment('user_activities')
+    #segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
@@ -28,12 +28,13 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
-    subsegment = xray_recorder.begin_segment('mock-data')
-    dict = {
-      "now": now.isoformat(),
-      "results-size": len(model['data'])
-    }
-    ubsegment.put_metadata('key', dict, 'namespace')
+      
+    #subsegment = xray_recorder.begin_segment('mock-data')
+    #dict = {
+     # "now": now.isoformat(),
+     # "results-size": len(model['data'])
+    #}
+    #subsegment.put_metadata('key', dict, 'namespace')
 
 
     return model
