ab7dc3ce55786ce3c4a0a0edfe4230148b576b52
Bonny-code <ndiboniface@outlook.com>
2023-04-12 20:06:22 +0000

    created typescript bucket

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    // The code that defines your stack goes here
+
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    
+  }
+}
