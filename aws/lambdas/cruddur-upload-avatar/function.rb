13d1134a59bdc154d6cecb3a09ecba5d864936aa
Bonny-code <ndiboniface@outlook.com>
2023-05-15 08:57:03 +0000

    try and upload avatar

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -10,7 +10,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-bonnycode-awsbootcampcr-34wfbgm9lqj.ws-eu96b.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-bonnycode-awsbootcampcr-k3etk3l11on.ws-eu97.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200
@@ -38,7 +38,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-bonnycode-awsbootcampcr-34wfbgm9lqj.ws-eu96b.gitpod.io",
+        "Access-Control-Allow-Origin": "https://3000-bonnycode-awsbootcampcr-k3etk3l11on.ws-eu97.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200, 
@@ -46,18 +46,3 @@ def handler(event:, context:)
     }
   end # if 
 end # def handler
-
-/*
-def handler(event:, context:)
-  puts event
-  s3 = Aws::S3::Resource.new
-  bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-  object_key = "mock.jpg"
-
-  obj = s3.bucket(bucket_name).object(object_key)
-  url = obj.presigned_url(:put, expires_in: 60 * 5)
-  url # this is the data that will be returned
-  body = {url: url}.to_json
-  { statusCode: 200, body: body}
-end
-*/
